f7ead319440d89ca0e4358bead806bc6a1fa2911
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-15 21:25:51 +0000

    explication des routes  et MVC en symfony

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <h2>Définition</h2
     <p>
       Une route est une adresse internet propre à notre site. Cette route est un point d'accès qui peut retourner unne page ou des données, par exemple. Pour naviguer dans notre site , nous avons besoin de définir un ensemble de routes.
+
+      Symfony est un framework HTTP dont le but principal est de transformer une requête en reponse .
+      pour cela , plusieurs architectures sont possibles, mais on utilisera le plus le plus répandu d'entre eux : le modele MVC qui convient perafaitement pour réaliser des applicationns web classiques.
+      C'est une organisation du code qui permet de correctement séparer loes responsabilités des options de codes de notre application. Les models servent à interagir avec notre base de données , les views contiennent le rendu visuel de nos pages et les controllers servent d'intermédiaire et pilotent les interactions entre toutes nos options de code. il est en  quelque sorte le chef d'orchestre de notre application.
       
     </p>
 <h1></h1>
