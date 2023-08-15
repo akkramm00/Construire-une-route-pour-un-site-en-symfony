be631169ead80f45bdbf6e4a108ca9824a31e5be
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-15 21:13:45 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Construire une route pour un  site en symfony</title>
+  </head>
+  <body>
+    <h2>Définition</h2
+    <p>
+      Une route est une adresse internet propre à notre site. Cette route est un point d'accès qui peut retourner unne page ou des données, par exemple. Pour naviguer dans notre site , nous avons besoin de définir un ensemble de routes.
+      
+    </p>
+<h1></h1>
+  </body>
+</html>
